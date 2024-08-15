536ed7346099dfb5f7ddcc09ca2d3a6875aedd11
tincho1980 <miramallo@gmail.com>
2024-08-15 15:07:07 -0300

    vue carrousel installed

---
+++ components/carrouselComponent.vue
@@ -0,0 +1,77 @@
+<template>
+    <Carousel :items-to-show="2.5" :wrap-around="true">
+        <Slide v-for="slide in data" :key="slide">
+            <div class="carousel__item">
+                <img :src="slide.download_url" alt="" />
+            </div>
+        </Slide>
+
+        <template #addons>
+            <Navigation />
+        </template>
+    </Carousel>
+</template>
+
+<script>
+import { Carousel, Navigation, Slide } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
+export default {
+    props: {
+        data: {
+            type: Array,
+            required: true,
+        },
+    },
+    components: {
+        Carousel,
+        Navigation,
+        Slide,
+    },
+    data() {
+        return {
+            settings: {
+                itemsToShow: 1,
+                snapAlign: "center",
+            },
+            // breakpoints are mobile first
+            // any settings not specified will fallback to the carousel settings
+            breakpoints: {
+                // 700px and up
+                700: {
+                    itemsToShow: 3.5,
+                    snapAlign: "center",
+                },
+                // 1024 and up
+                1024: {
+                    itemsToShow: 5,
+                    snapAlign: "start",
+                },
+            },
+        };
+    },
+};
+</script>
+
+<style scoped>
+.carousel__item {
+    min-height: 200px;
+    width: 100%;
+    background-color: var(--vc-clr-primary);
+    color: var(--vc-clr-white);
+    font-size: 20px;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.carousel__slide {
+    padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+    box-sizing: content-box;
+    border: 5px solid white;
+}
+</style>
--- pages/galeria.vue
+++ pages/galeria.vue
@@ -1,12 +1,59 @@
 <template>
-    <div>
-        <h2>Galería</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quas error ratione asperiores unde accusamus dolore obcaecati at nisi non!</p>
+    <div class="mx-16 my-6">
+        <ModalComponent :photoUrl="photoUrl" v-if="photoUrl" @click="closeModal()" />
+        <customTitle title="Galería" />
+        <SelectedButtons @selected="selectPortfolio" :startWith="startWith" />
+
+        <div class="mx-2 md:mx-16">
+            <CarrouselComponent :data="photos" />
+
+            <!-- <div class="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-3 w-auto mx-auto space-y-3 p-6 bg-white">
+                <div class="break-inside-avoid" v-for="photo in selectedPortfolio" :key="photo.id">
+                    <img class="h-auto rounded-lg" :src="photo.url" :alt="photo.name" @click="showPhoto(photo.url)" />
+                </div>
+            </div> -->
+        </div>
     </div>
 </template>
 
 <script>
-export default {};
+import SelectedButtons from "@/components/selectedButtons.vue";
+import ModalComponent from "@/components/modalComponent.vue";
+import CarrouselComponent from "@/components/carrouselComponent.vue";
+
+import galerias from "@/helpers/galerias.json";
+
+export default {
+    components: {
+        SelectedButtons,
+        ModalComponent,
+        CarrouselComponent,
+    },
+    data() {
+        return {
+            selectedPortfolio: null,
+            photos: galerias.bandas[1].fotos,
+            startWith: "bandas",
+        };
+    },
+    methods: {
+        selectPortfolio(id) {
+            this.selectedPortfolio = galerias[id];
+        },
+        showPhoto(url) {
+            this.photoUrl = url;
+        },
+        closeModal() {
+            this.photoUrl = null;
+        },
+    },
+    beforeMount() {
+        if (this.$route.query.startWith) {
+            this.startWith = this.$route.query.startWith;
+        }
+        this.selectPortfolio(this.startWith);
+    },
+};
 </script>
 
-<style></style>
+<style scoped></style>
